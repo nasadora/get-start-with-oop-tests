0b54233104e81824ae58be41a9ef5c1f47eb2ef7
Adora <nasadora_mohddan@hotmail.com>
2018-10-09 12:08:08 +0800

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace GetStartWithOOPTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var emps = GetHRSampleDataWithID();
+            foreach (var emp in emps)
+            {
+                Console.WriteLine(HR.Utilites.CommonUtility.GetFullname(emp));
+            }
+            Console.WriteLine();
+            foreach (var emp in emps)
+            {
+                Console.WriteLine(emp.GetFullname());
+            }
+        }
+    }
+}
